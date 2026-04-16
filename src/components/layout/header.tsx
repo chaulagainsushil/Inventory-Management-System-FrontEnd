1cc0bdec392657c831180d3cd74569dceafb8fab
Sushil Chaulagain <Sushilchaulagain25@gmail.com>
2026-04-16 00:26:09 +0000

    remove this section  (_for element <Input>_)

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -81,17 +81,7 @@ export default function Header() {
       <div className="hidden md:block">
         <h1 className="text-lg md:text-2xl font-semibold">Welcome Back, {user?.name || 'User'}</h1>
       </div>
-      <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-        <form className="ml-auto flex-1 sm:flex-initial">
-          <div className="relative">
-            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-            <Input
-              type="search"
-              placeholder="Search products..."
-              className="pl-8 w-full sm:w-[300px] md:w-[200px] lg:w-[300px]"
-            />
-          </div>
-        </form>
+      <div className="flex w-full items-center justify-end gap-4 md:ml-auto md:gap-2 lg:gap-4">
         <DropdownMenu onOpenChange={(open) => {
             if(open) {
                 setNotificationLoading(true);
